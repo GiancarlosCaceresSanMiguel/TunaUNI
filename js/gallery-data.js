fc9a0e837847cc92971b3e8a516d39fea101c44f
GiancarlosCaceresSanMiguel <giancarlos.caceres.s@uni.pe>
2026-09-21 19:44:56 -0500

    Add files via upload

---
+++ js/gallery-data.js
@@ -0,0 +1,165 @@
+window.galleryAlbums = {
+  "2026": {
+    "label": "2026",
+    "months": [
+      {
+        "name": "Enero",
+        "images": []
+      },
+      {
+        "name": "Febrero",
+        "images": []
+      },
+      {
+        "name": "Marzo",
+        "images": []
+      },
+      {
+        "name": "Abril",
+        "images": []
+      },
+      {
+        "name": "Mayo",
+        "images": []
+      },
+      {
+        "name": "Junio",
+        "images": []
+      },
+      {
+        "name": "Julio",
+        "images": []
+      },
+      {
+        "name": "Agosto",
+        "images": [
+          {
+            "src": "img/galeria/2026/Agosto/CertamanArequipa9.jpg",
+            "description": "Presentación de la Tuna UNI durante el certamen de Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa.jpeg",
+            "description": "Participación de la Tuna UNI en el certamen de Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa10.jpg",
+            "description": "Recuerdo de la Tuna UNI en el certamen de Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa2.jpg",
+            "description": "Presentación musical de la Tuna UNI en Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa3jpg.jpg",
+            "description": "Momento especial de la Tuna UNI durante el certamen."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa4.jpg",
+            "description": "Recuerdo de la participación de la Tuna UNI en Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa5.jpg",
+            "description": "Presentación de la Tuna UNI en una jornada musical en Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa6.jpg",
+            "description": "La Tuna UNI compartiendo música y tradición en Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa7.jpg",
+            "description": "Recuerdo de la Tuna UNI durante el encuentro de Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa8.jpg",
+            "description": "Participación de la Tuna UNI en una presentación en Arequipa."
+          },
+          {
+            "src": "img/galeria/2026/Agosto/CertamenArequipa9.jpg",
+            "description": "Recuerdo de la Tuna UNI durante el certamen de Arequipa."
+          }
+        ]
+      },
+      {
+        "name": "Septiembre",
+        "images": [
+          {
+            "src": "img/galeria/2026/Septiembre/BienvenidaCachimbosFIIS.jpg",
+            "description": "Bienvenida de cachimbos de la FIIS junto a la Tuna UNI."
+          },
+          {
+            "src": "img/galeria/2026/Septiembre/BienvenidaCachimbosFIIS2.jpg",
+            "description": "Presentación de la Tuna UNI durante la bienvenida de cachimbos de la FIIS."
+          },
+          {
+            "src": "img/galeria/2026/Septiembre/BienvenidaCachimbosFIISCEFIIS.jpeg",
+            "description": "Bienvenida de cachimbos de la CEFIIS junto a la Tuna UNI."
+          },
+          {
+            "src": "img/galeria/2026/Septiembre/CumpleañosProfesoresFIM.jpeg",
+            "description": "Celebración del cumpleaños de los profesores de la FIM junto a la Tuna UNI."
+          }
+        ]
+      },
+      {
+        "name": "Octubre",
+        "images": []
+      },
+      {
+        "name": "Noviembre",
+        "images": []
+      },
+      {
+        "name": "Diciembre",
+        "images": []
+      }
+    ]
+  },
+  "antiguas": {
+    "label": "Fotos antiguas",
+    "months": [
+      {
+        "name": "Archivo histórico",
+        "images": [
+          {
+            "src": "img/galeria/antiguas/Fundadores1.jpeg",
+            "description": "Recuerdo de los fundadores de la Tuna UNI."
+          },
+          {
+            "src": "img/galeria/antiguas/Fundadores2.jpeg",
+            "description": "Fotografía histórica de los fundadores de la Tuna UNI."
+          },
+          {
+            "src": "img/galeria/antiguas/Fundadores3.jpeg",
+            "description": "Momento histórico de la Tuna UNI junto a sus fundadores."
+          },
+          {
+            "src": "img/galeria/antiguas/Fundadores4.jpeg",
+            "description": "Recuerdo de la historia y tradición de la Tuna UNI."
+          },
+          {
+            "src": "img/galeria/antiguas/Fundadores5.jpeg",
+            "description": "Imagen histórica de la Tuna UNI y sus primeros integrantes."
+          },
+          {
+            "src": "img/galeria/antiguas/Fundadores6.jpeg",
+            "description": "Recuerdo de los inicios de la Tuna UNI."
+          }
+        ]
+      }
+    ]
+  }
+};
+window.galleryRecognitions = [
+  {
+    "src": "img/reconocimientos/MejorBaileBanderaArequipa.jpg",
+    "description": "Premio obtenido por la Tuna UNI en Arequipa por el mejor baile de bandera."
+  },
+  {
+    "src": "img/reconocimientos/MejorBailePanderoArequipa.jpg",
+    "description": "Premio obtenido por la Tuna UNI en Arequipa por el mejor baile de pandero."
+  },
+  {
+    "src": "img/reconocimientos/MejorSolistaArequipa.jpg",
+    "description": "Premio obtenido por la Tuna UNI en Arequipa por el mejor solista."
+  }
+];
